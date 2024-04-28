fcf527c22324d5a8295715f6e49996ed76e11146
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-04-28 05:24:29 +0100

    card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -9,7 +9,25 @@ const Card = () => {
 					<img src={leave} alt='leave' />
 				</div>
 				<div>
-					<h2 className='mission' style={{marginTop:"-20px"}}>Mission and Goals</h2>
+          <h2 className='mission' style={{ marginTop: "-20px" }}>Mission and Goals</h2>
+          
+"use client";
+
+import { Card } from "flowbite-react";
+
+export function Component() {
+
+    <Card href="#" className="max-w-sm">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
+    </Card>
+  
+
+
 				</div>
 				<div>
 					<img src={leave} alt='leave' />
