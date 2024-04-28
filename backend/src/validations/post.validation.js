f98eb608bd3f7dc760e69cd327302881b967c7fd
H-Asmaa <asmaehadar32@gmail.com>
2024-04-28 03:01:16 +0100

    Committing the post setup, adding all the CRUD related to post.
    Create post, delete post, update and delete post.

---
+++ backend/src/validations/post.validation.js
@@ -0,0 +1,53 @@
+const Joi = require('joi');
+const { objectId } = require('./custom.validation');
+
+const createPost = {
+  body: Joi.object().keys({
+    title: Joi.string().required(),
+    content: Joi.string().required(),
+    author: Joi.string().custom(objectId),
+  }),
+};
+
+const getPosts = {
+  query: Joi.object().keys({
+    title: Joi.string(),
+    author: Joi.string().custom(objectId),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getPost = {
+  params: Joi.object().keys({
+    postId: Joi.string().custom(objectId),
+  }),
+};
+
+const updatePost = {
+  params: Joi.object().keys({
+    postId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      title: Joi.string(),
+      content: Joi.string(),
+      author: Joi.string().custom(objectId),
+    })
+    .min(1),
+};
+
+const deletePost = {
+  params: Joi.object().keys({
+    postId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createPost,
+  getPosts,
+  getPost,
+  updatePost,
+  deletePost,
+};
