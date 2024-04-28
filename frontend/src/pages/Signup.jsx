1c3ef0b9db6614fb498ed50b400a7d2134ed358b
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-04-28 09:44:52 +0100

    modified

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -9,27 +9,31 @@ const Signup = () => {
 			<Navbar />
 			<main className='bg-emerald-800 p-6'>
 				<div className='flex items-center justify-center'>
-					<form className='flex flex-col gap-4 signupbg'>
+					<form className='flex flex-col items-center justify-center gap-4 signupbg w-80'>
+						<div>
+							<h2 className='text-3xl'>HelpBirdy</h2>
+							<h3 className='text-center'>Signup</h3>
+						</div>
 						<div>
 							<div className='mb-2 block'>
-								<Label htmlFor='name' value='FullName' />
+								<Label htmlFor='name' className='font-bold' value='FullName' />
 							</div>
 							<TextInput
 								id='name'
 								type='text'
-								className='rounded-lg p-2'
+								className='rounded-lg p-2 w-72'
 								placeholder='Jon Doe'
 								required
 							/>
 						</div>
 						<div>
 							<div className='mb-2 block'>
-								<Label htmlFor='email' value='Email' />
+								<Label htmlFor='email' className='font-bold' value='Email' />
 							</div>
 							<TextInput
 								id='email'
 								type='email'
-								className='rounded-lg p-2'
+								className='rounded-lg p-2 w-72'
 								placeholder='Jon@gmail.com'
 								required
 							/>
@@ -37,19 +41,24 @@ const Signup = () => {
 
 						<div>
 							<div className='mb-2 block'>
-								<Label htmlFor='password1' value='Your password' />
+								<Label
+									htmlFor='password1'
+									className='font-bold'
+									value='Your password'
+								/>
 							</div>
 							<TextInput
 								id='password1'
-								className='rounded-lg p-2'
+								className='rounded-lg p-2 w-72'
 								type='password'
 								required
 							/>
 						</div>
-
-						<Button type='submit' className='signBtn'>
-							Signup
-						</Button>
+						<div>
+							<Button type='submit' className='signupBtn'>
+								Signup
+							</Button>
+						</div>
 					</form>
 				</div>
 			</main>
