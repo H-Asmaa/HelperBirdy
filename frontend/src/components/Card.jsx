00deab02715a9e97871cc37ac79f3241d6dcbd08
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-04-28 05:22:34 +0100

    card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,15 +3,18 @@ import leave  from "../assets/leaves.png"
 const Card = () => {
   return (
 		<main>
-			<div className='flex justify-between items-center'>
+			<div
+				className='flex justify-center items-center mt-10'>
 				<div>
 					<img src={leave} alt='leave' />
 				</div>
 				<div>
+					<h2 className='mission' style={{marginTop:"-20px"}}>Mission and Goals</h2>
+				</div>
+				<div>
 					<img src={leave} alt='leave' />
 				</div>
-      </div>
-      
+			</div>
 		</main>
 	);
 }
