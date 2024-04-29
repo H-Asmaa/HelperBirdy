44fa3bc0757b31d845fde84d2ddd58c98edde24f
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-04-29 12:51:28 +0100

    style: modifed the Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
-
-import honesty from "../assets/Honesty.png"
-
-
+import honesty from '../assets/Honesty.png';
+import logo from '../assets/HelperBirdy.png';
 
 export default function Navbar() {
 	const [open, setOpen] = useState(false);
@@ -13,44 +11,50 @@ export default function Navbar() {
 	}
 
 	return (
-		<header className='bg-emerald-800 text-white active:bg-yellow-700'>
-			<div className='flex justify-between items-center xl:max-7xl xl:mx-auto py-5 px-[8%] w-full flex-wrap'>
-				<a href='/'>
-					<h1 className='flex lg:text-2xl items-center cursor-pointer logo'>
-						HelperBirdy
-					</h1>
-				</a>
+		<header className='bg-white lg:bg-emerald-800'>
+			<nav className='flex justify-between items-center xl:max-7xl xl:mx-auto py-5 px-[8%] w-full flex-wrap'>
+				<div className='hidden lg:block'>
+					<a href='/' className='flex items-center cursor-pointer mt-2'>
+						<img
+							src={logo}
+							alt='the logo of helperbird which shows the context of the application'
+							className='w-20'
+						/>
+						<h1 className='text-2xl font-semibold logo'>HelperBirdy</h1>
+					</a>
+				</div>
+
 				<FiMenu
 					className='lg:hidden block cursor-pointer h-6 w-6'
 					onClick={Modal}
 				/>
-				<nav
+				<div
 					className={`lg:flex lg:items-center lg:w-auto w-full ${
 						open ? 'block' : 'hidden'
 					}`}>
-					<ul className='lg:flex lg:justify-between items-center'>
+					<ul className='bg-yellow lg:flex lg:justify-between items-center mt-4 lg:mt-0'>
 						<li className='list'>
 							<a href='/'>Home</a>
 						</li>
-						<li className='list'>
+						<li className='list ml-0 lg:ml-4'>
 							<a href='/queue'>Queue</a>
 						</li>
-						<li className='list'>
+						<li className='list ml-0 lg:ml-4'>
 							<a href='/news'>NewsLetter</a>
 						</li>
-						<li className='list'>
+						<li className='list ml-0 lg:ml-4'>
 							<a href='/login'>Login</a>
 						</li>
-						<li className='list'>
+						<li className='list ml-0 lg:ml-4'>
 							<a href='/signup'>Signup</a>
 						</li>
 					</ul>
-				</nav>
+				</div>
 				<button className='flex items-center NavBtn'>
-					<img src={honesty} alt='a helping hand' className='w-10' />
-					Donate
+					<img src={honesty} alt='a helping hand' className='w-10 mr-1' />
+					<span>Donate</span>
 				</button>
-			</div>
+			</nav>
 		</header>
 	);
 }
