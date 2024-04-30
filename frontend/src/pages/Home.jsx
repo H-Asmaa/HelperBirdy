6ce2673e82fcb2e5e39cea7711ce8cd5bddcb98c
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-04-30 13:09:33 +0100

    style: modified components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 		<>
 			<Navbar />
 			<main className='bg-emerald-800 '>
-				<div className='grid grid-cols-1 md:grid-cols-2 max-w-7xl mx-auto p-10'>
+				<div className='grid grid-cols-1 md:grid-cols-2 max-w-7xl mx-auto p-6 lg:p-10'>
 					<section className='text-white text-justify ml-5'>
 						<h2 className='head'>
 							Your Contribution has
