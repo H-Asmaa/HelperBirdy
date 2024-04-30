9dc07becc2d3436b57a9b4b3992e4c71b4d05750
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-04-30 13:33:27 +0100

    modified

--- frontend/src/components/Help.jsx
+++ frontend/src/components/Help.jsx
@@ -3,14 +3,13 @@ import Button from "./Button"
 
 const Help = () => {
   return (
-    <div className="bg-orange-500 text-white p-8">
+    <div className=" help-container">
       <div className="flex flex-col items-center justify-center">
-        <h3 className="mb-3 text-lg lg:text-2xl">Be helpful as you want to be</h3>
-        <a href="/donate">
+        <h3 className="help-title">Be as helpful as you want to be</h3>
+        <a href="/donate" className="shadow-2xl">
           <Button value="Join Help Birdy" className="helpbtn"/>
         </a>
       </div>
-      
     </div>
   )
 }
