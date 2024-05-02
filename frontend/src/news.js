417c6930cd4b426244278eabf618dffd84213169
Ijeoma Igboagu <ijeonyi@gmail.com>
2024-05-02 10:11:05 +0100

    Remove image import and update News page to display news data from state
    The diff removes the import of the 'man.png' image and updates the 'News.jsx' component to use news data from a state variable called 'news

---
+++ frontend/src/news.js
@@ -0,0 +1,20 @@
+export const data = [
+	{
+		id: 1,
+		img1: '/Bookmark.png',
+		img2: '/man.png',
+		title: 'Rojer’s experience with volunteering',
+		text1: 'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
+    text2: 'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
+	},
+
+	{
+		id: 2,
+		img1: '/Bookmark.png',
+		img2: '/man.png',
+		title: 'Rojer’s experience with volunteering',
+		text1: 'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
+    text2: 'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
+	},
+
+];
--- frontend/src/queue.js
+++ frontend/src/queue.js
@@ -4,14 +4,20 @@ export const data = [
 		img1: '/Bookmark.png',
 		img2: '/volunteers.png',
 		title: 'Your contribution has an effect',
-		text: 'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
+		text1:
+			'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
+		text2:
+			'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
 	},
 	{
 		id: 2,
 		img1: '/Bookmark.png',
 		img2: '/volunteers.png',
 		title: 'Your contribution has an effect',
-		text: 'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
+		text1:
+			'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
+		text2:
+			'Welcome to HelperBird, where passion meets purpose! Our platform connects volunteers with organizations and causes in need, empowering individuals to make a meaningful impact with their skills and expertise.',
 	},
 	{
 		id: 3,
